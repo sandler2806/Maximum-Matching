fbd25cf4e22b0bbec0c93f70dfb4a0aeb2feda90
Yuval sandler <sandler2806@gmai.com>
2022-04-12 17:58:53 +0300

    add functions

--- src/MaximumMatching.py
+++ src/MaximumMatching.py
@@ -5,6 +5,22 @@ from Node import Node
 import copy
 
 
+def createPath(node: Node) -> list:
+    path = [node]
+    while node.parent is not None:
+        node = node.parent
+        path.append(node)
+    path.reverse()
+    return path
+
+
+def alternatePath(path: list[Node]):
+    for i in range(0, len(path), 2):
+        node = path[i]
+        node.match = path[i + 1]
+        path[i + 1].match = node
+
+
 class MaximumMatching:
 
     def __init__(self, graph: Graph):
@@ -12,7 +28,7 @@ class MaximumMatching:
         self.graph = graph
         self.cycle: List[Node] = []
         self.exposed = []
-
+        self.blossoms = []
 
     def constract_blossom(self, blossom_nodes: list):
         self.cycle = blossom_nodes
@@ -29,53 +45,51 @@ class MaximumMatching:
                         blossom.match = temp_node
         for node in blossom_nodes:
             self.graph.remove_node(node.key)
+
     def distract_blossom(self, blossom_node: Node):
         self.graph = copy.deepcopy(self.orgGraph)
         node_neigh: Node = self.graph.nodes.get(blossom_node.match.key)
         for neigh in node_neigh.edges:
             if neigh in self.cycle:
-
-
-
-
-
-
-
-
-
+                pass
 
     def findMatching(self):
         self.findExposed()
-        for node in self.exposed:
-            self.resetNodes()
-            path = self.findAugmentingPath(node)
-            if len(path) == 0:
-                pass
+        augmentingPathFound = True
+        while augmentingPathFound:
+            augmentingPathFound = False
+            for node in self.exposed:
+                self.resetNodes()
+                path = self.findAugmentingPath(node)
+                if len(path) > 0:
+                    augmentingPathFound = True
+                    alternatePath(path)
+                    break
 
     def findAugmentingPath(self, src: Node) -> list:
         queue: Queue[Node] = Queue()
         queue.put(src)
         while not queue.empty():
             currNode = queue.get()
+            currNode.visited = True
             if currNode.parent is None or currNode.parent.match == currNode:
                 for neiId in currNode.edges:
                     nei = self.graph.nodes.get(neiId)
-                    if currNode.parent == nei:
+
+                    if currNode.parent == nei or nei.visited is True:
                         continue
                     nei.parent = currNode
 
                     if nei in self.exposed:
-                        path = [nei]
-                        while nei.parent is not None:
-                            nei = nei.parent
-                            path.append(nei)
-                        return path.reverse()
-
-                    if (currNode.match is None and nei.match is not None) or \
-                            (currNode.match is not None and nei.match is None):
-                        queue.put(nei)
+                        return createPath(nei)
+                    queue.put(nei)
             else:
-                pass
+                nei = currNode.match
+                nei.parent = currNode
+                if nei.visited is False:
+                    if nei in self.exposed:
+                        return createPath(nei)
+                queue.put(nei)
 
     def findExposed(self):
         for node in self.graph.nodes.values():
@@ -86,4 +100,3 @@ class MaximumMatching:
         for node in self.graph.nodes.values():
             node.parent = None
             node.visited = None
-            # self.findExposed()
